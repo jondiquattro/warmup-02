f2a02d179f55ffdcf0f4ef71495bbf82d6feae92
Jonathan DiQuattro <jondiquattro@hotmail.com>
2019-01-29 20:25:14 -0800

    daily

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 'use strict';
 
 const express = require('express');
+const routes = require('./routes/routes.js');
 
-const app = express;
+let app = express();
 
-router = require('./routes/routes.js');
+//-------------ROUTES------------------//
 
-app.use(router);
-// app.length('/', (req,res,next)=>{
-//     res.send('hello world');
+// app.get('/', getRoutes);
+
+// router.post('/:item', postFile);
+app.use(routes);
+
+
+
+//------------Helpers----------------//
+// function getRoutes(req,res,next){
+//     res.send('hello world')
+//     .catch(error)
+// }
+
+
+// function postFile(req,res,next){
+//    console.log('called from post');
+// }
+
+
+
+
+// app.listen(process.env.PORT, ()=>{
+//     console.log(`up on port ${PORT}`);
 // })
 
-
-
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`listening on port ${PORT}`);
+app.listen(3000, ()=>{
+    console.log(`up on port 3000`);
 })
--- routes/routes.js
+++ routes/routes.js
@@ -5,8 +5,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', hello);
-router.use(express.static('/public'));
+// router.use(express.static('/public'));
+router.post('/save', jsonResponse)
 
 function hello(req,res,next){
     res.send('hello');
 }
+
+function jsonResponse(req,res,next){
+    console.log(req.body)
+    .then(next)
+}
+module.exports = router;
